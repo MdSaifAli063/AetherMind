17cae142807ce0b64e2352c852e9e41a405323e3
Mdsaif4363 <mdsaifali6303@gmail.com>
2026-04-22 12:38:33 +0530

    added

---
+++ openenv_env/env.py
@@ -0,0 +1,24 @@
+import random
+from .memory import Memory
+from .reward import reward_function
+from .schema_manager import SchemaManager
+
+class LifeOSEnv:
+    def __init__(self, scenarios):
+        self.scenarios = scenarios
+        self.memory = Memory()
+        self.schema_manager = SchemaManager()
+
+    def reset(self):
+        self.current = random.choice(self.scenarios)
+        self.state = self.schema_manager.apply_schema(self.current)
+        return self.state
+
+    def step(self, action):
+        reward = reward_function(action, self.current)
+
+        # store in memory
+        self.memory.add(self.current, action)
+
+        done = True
+        return self.state, reward, done, {}
